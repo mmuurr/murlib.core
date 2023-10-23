83ce36963e88675a2ba7a50c513ed0c2448ce6dd
murman <mmuurr@gmail.com>
2023-10-23 18:14:05 +0200

    coalesce bugfix for matrices

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,15 +1,26 @@
+##==============================================================================
+## Previous versions of the code below looked like so:
+##   ifelse(is.na(x), x, replacement)  # for na2
+## But ifelse (annoyingly) declasses matrices :-|.
+## Hence the explicit bracketed assignment versions seen below.
+## (There's a warning about this behavior in the ifelse reference page.)
+##==============================================================================
+
 #' @export
 na2 <- function(x, replacement) {
-    ifelse(is.na(x), x, replacement)
+  x[is.na(x)] <- replacement
+  x
 }
 
 #' @export
 nan2 <- function(x, replacement) {
-    ifelse(is.nan(x), x, replacement)
+  x[is.nan(x)] <- replacement
+  x
 }
 
 #' @export
 inf2 <- function(x, replacement) {
-    ifelse(is.infinite(x), x, replacement)
+  x[is.infinite(x)] <- replacement
+  x
 }
 
