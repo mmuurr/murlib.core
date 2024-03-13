0fe3fd84335ff5b6fed297c2fd2a35e65e2af983
murman <mmuurr@gmail.com>
2024-03-13 13:47:59 +0100

    use explicit handling for adding class to NULL

--- R/class.R
+++ R/class.R
@@ -14,15 +14,27 @@ inherits_all <- function(x, klass = character(0)) {
 }
 
 
+## Because consider x <- 1
+## class(x) == "integer", but attr(x, "class") is NULL.
+## We should preserve the NULLness, I think.
+## The class(x) is fallback behavior when the "class" attribute is NULL.
+## By assigning a class explicitly, we are bypassing this fallback.
+class_attr <- function(x) attr(x, "class", TRUE)
+
+
+#' @param null replace `NULL` with this val. If `NULL`, then `NULL` is returned (without a class).
 #' @export
-prepend_class <- function(x, klass = character(0), force = FALSE) {
-  if (inherits_all(x, klass) && !isTRUE(force)) return(x)
-  structure(x, class = unique(c(klass, class(x))))
+prepend_class <- function(x, cls = character(0), null = NULL) {
+  if (is.null(x)) x <- null  ## x is NULL? replace with `null`.
+  if (is.null(x)) return(NULL)  ## x is still NULL? return it.
+  structure(x, class = unique(c(cls, class_attr(x))))
 }
 
 
+#' @param null replace `NULL` with this val. If `NULL`, then `NULL` is returned (without a class).
 #' @export
-append_class <- function(x, klass = character(0), force = FALSE) {
-  if (inherits_all(x, klass) && !isTRUE(force)) return(x)
-  structure(x, class = unique(c(class(x), klass)))
+append_class <- function(x, cls = character(0), null = NULL) {
+  if (is.null(x)) x <- null  ## x is NULL? replace with `null`.
+  if (is.null(x)) return(NULL)  ## x is still NULL? return it.
+  structure(x, class = unique(c(class_attr(x), cls)))
 }
