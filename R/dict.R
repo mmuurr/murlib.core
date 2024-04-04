d19048c1b290ce433e463555c95dadae149336e8
Murat Tasan <mmuurr@gmail.com>
2024-04-05 01:51:50 +0200

    export is_dict

--- R/dict.R
+++ R/dict.R
@@ -1,5 +1,4 @@
-#' @export
-dict <- (function() {
+foo <- (function() {
 
   ## rlang:::detect_void_name
   detect_void_name <- function(x) {
@@ -31,10 +30,20 @@ dict <- (function() {
     is_named(x) && !any(duplicated(names(x)))
   }
 
-  function(...) {
+  dict <- function(...) {
     if (...length() == 0) return(structure(list(), names = character(0)))
     x <- list(...)
     if (!isTRUE(is_dict(x))) stop("not dictionaryish")
     x
   }
+
+  list(dict = dict, is_dict = is_dict)
 })()
+
+#' @export
+dict <- foo$dict
+
+#' @export
+is_dict <- foo$is_dict
+
+rm(foo)
